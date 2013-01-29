a43f3d78c49ed87fe53a9d4cdda0f35ea1b668b8
Gregory Jordan <gjuggler@gmail.com>
2013-01-29 13:03:03 -0700

    Fix up get_species script (ready for filling in the API fetching code)

--- cgi-bin/get_species.pl
+++ cgi-bin/get_species.pl
@@ -11,7 +11,6 @@ use CGI;
 use LWP::UserAgent;
 use JSON;
 use Log::Log4perl qw(:easy);
-use HelperMethods;
 use File::Spec::Functions qw(catfile);
 use Data::DPath qw(dpath);
 use File::Slurp;
@@ -31,11 +30,13 @@ external APIs.
 
     OPTIONS:
 
-    --latitude <Number>           Latitude of the center point to search
-    --longitude <Number>      Longitude of the center point to search
-    --radius <Number>         Radius around which to search
-    --service <String>         Which service to use (options: "inaturalist", "mapoflife")
-    --help                 show this help message
+    --latitude <Number>       Latitude of the center point to search (or, when using iNaturalist, the latitude of the SW corner)
+    --longitude <Number>      Longitude of the center point to search (or, when using iNaturalist, the longitude of the SW corner)
+    --radius <Number>         Radius around which to search (only applicable if using map of life)
+    --ne_latitude <Number>    Latitude of the NE corner (only applicable if using iNaturalist)
+    --ne_longitude <Number>   Longitude of the NE corner (only applicable if using iNaturalist)
+    --service <String>        Which service to use (options: "inaturalist", "mapoflife")
+    --help                    Show this help message
 
 INVOKING THROUGH CGI:
 
@@ -72,6 +73,8 @@ Log::Log4perl::easy_init(IS_CGI ? $WARN : $INFO);
 my $latitude = 40;
 my $longitude = -109;
 my $radius = 10;
+my $ne_latitude = 50;
+my $ne_longitude = -100;
 my $service = 'inaturalist';
 my $help_opt = 0;
 
@@ -80,11 +83,15 @@ my $cgi = CGI->new();
 if (IS_CGI) {
     $latitude = $cgi->param('latitude') if $cgi->param('latitude');
     $longitude = $cgi->param('longitude') if $cgi->param('longitude');
+    $ne_latitude = $cgi->param('ne_latitude') if $cgi->param('ne_latitude');
+    $ne_longitude = $cgi->param('ne_longitude') if $cgi->param('ne_longitude');
     $service = $cgi->param('service') if $cgi->param('service');
 } else {
     my $getopt_success = GetOptions(
         'latitude=s' => \$latitude,
         'longitude=s' => \$longitude,
+        'ne_latitude=s' => \$ne_latitude,
+        'ne_longitude=s' => \$ne_longitude,
         'radius=s' => \$radius,
         'service=s' => \$service,
         'help' => \$help_opt,
@@ -100,22 +107,62 @@ if (IS_CGI) {
 # main
 #----------------------------------------------------------------------
 
+my @species;
+
 if ($service eq 'inaturalist') {
-    INFO(sprintf('Fetching species list from iNaturalist...'));
+   INFO(sprintf('Fetching species list from iNaturalist...'));
+
+   @species = search_inaturalist($latitude, $longitude, $ne_latitude, $ne_longitude);
+
+} elsif ($service eq 'mapoflife') {
+   INFO(sprintf('Fetching species list from map of life...'));
+
+   @species = search_map_of_life($latitude, $longitude, $radius);
 }
 
 print $cgi->header(-status => 200, -type => 'text/plain') if IS_CGI;
-print "DONE!";
+
+#@species = ('Mus musculus', 'Homo sapiens');
+
+output_species(@species);
+
 
 #----------------------------------------------------------------------
 # helper routines
 #----------------------------------------------------------------------
 
+
+# Prints the given list of species, separated by newlines.
+sub output_species {
+  my @species = @_;
+  foreach my $s (@species) {
+    print $s."\n";
+  }
+}
+
 sub search_inaturalist {
-  my $query = shift;
+    my ($latitude, $longitude, $ne_longitude, $ne_longitude) = @_;
+
+    # CODE TO FETCH SPECIES FROM INATURALIST GOES HERE
 
 }
 
 sub search_map_of_life {
-  my $query = shift;
+  my ($latitude, $longitude, $radius) = @_;
+
+  # CODE TO FETCH SPECIES FROM MAP OF LIFE GOES HERE
+
+}
+
+# a 'die' method that works in both CGI and commandline context
+sub fatal {
+    my ($msg, $is_cgi, $http_status) = @_; 
+    if ($is_cgi) {
+        $http_status ||= 500;
+        print CGI->header(-status => $http_status, -type => 'text/plain');
+        print $msg;
+        exit 0;
+    } else {
+        die "$msg\n";
+    }
 }
