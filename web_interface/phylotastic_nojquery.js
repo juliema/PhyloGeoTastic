cb806a6784a1cacea5cdc0c2b0f0feea4743107b
Gregory Jordan <gjuggler@gmail.com>
2013-06-14 15:48:56 -0400

    Add compiled JS and CSS.

--- web_interface/phylotastic_nojquery.js
+++ web_interface/phylotastic_nojquery.js
@@ -2230,10 +2230,10 @@ Phylotastic.App = {
     var species = Phylotastic.DataSources.species;
     var allowedSpecies = curSource.allowedSpeciesFilters;
 
-    for (var i=0; i < species.length; i++) {
+    for (var i = 0; i < species.length; i++) {
       var x = species[i];
       x.button.addClass('disabled');
-      for (var j=0; j < allowedSpecies.length; j++) {
+      for (var j = 0; j < allowedSpecies.length; j++) {
         var y = allowedSpecies[j];
         if (x.id === y) {
           x.button.removeClass('disabled');
@@ -2244,7 +2244,9 @@ Phylotastic.App = {
     Phylotastic.Maps.clearOverlays();
   },
 
-  updateMapToSpecies: function() {},
+  updateMapToSpecies: function() {
+
+  },
 
   getWaitingHtml: function() {
     var source = Phylotastic.DataSources.currentSource;
@@ -2270,6 +2272,7 @@ Phylotastic.App = {
 
     // Get the map parameters to send to the server-side script.
     var mapParams = Phylotastic.Maps.currentParams || {};
+    console.log("PARAMS", mapParams);
     var params = {
       service: source.id,
       species_group: species.id
@@ -2295,6 +2298,16 @@ Phylotastic.App = {
 
         //console.log("Species", species.length);
         var msg;
+
+        // Create a map of all latin names to thumbnail URLs.
+        me.speciesThumbMap = {};
+
+        for (var i = 0; i < species.length; i++) {
+          var x = species[i];
+          if (x.thumbnail) {
+            me.speciesThumbMap[x.taxon_name] = x.thumbnail;
+          }
+        }
 
         if (species.length > 0) {
           var exampleImages = [];
@@ -2341,8 +2354,8 @@ Phylotastic.App = {
           msg = [
             'Found ' + allSpecies.length + ' species!',
             exampleText,
-//            '<div>To view a list of all the species, <a href="#" id="send-ptastic-query">click here</a>.</div>',
-            '<div>To explore their evolutionary relationships, <a href="#" id="send-ptastic-query">click here</a>.</div>'].join('');
+            //            '<div>To view a list of all the species, <a href="#" id="send-ptastic-query">click here</a>.</div>',
+            '<div>To explore their evolutionary relationships, <a class="pgt-link" id="send-ptastic-query">click here</a>.</div>'].join('');
         } else {
           msg = '<p>No results found. Try a broader search.</p>';
         }
@@ -2352,10 +2365,8 @@ Phylotastic.App = {
 
         //console.log("All species:");
         //console.log(allSpecies.join('\n'));
-
         //console.log("All common names:");
         //console.log(allCommonNames.join('\n'));
-
         $('#send-ptastic-query').on('click', function() {
           me.sendPhyloTasticQuery(allSpecies);
         });
@@ -2364,6 +2375,8 @@ Phylotastic.App = {
   },
 
   sendPhyloTasticQuery: function(species) {
+    var me = this;
+
     var contactingNext = [
       '<p>Contacting Phylo<em>tastic</em> to find evolutionary relationships...</p>',
       '<p>This may take a while, please be patient.</p>'].join('');
@@ -2387,19 +2400,33 @@ Phylotastic.App = {
         tree = tree.replace(/&quot;/g, '');
         tree = tree.replace(/"/g, '');
 
-        var url = ['./treeView.html?tree=',
-                   tree+'',
-                   ''].join('');
+        var thumbs = me.speciesThumbMap;
 
-        var gotText = [
-          '<p>Found the evolutionary tree connecting your species!',
-          '</p><p>',
-          '<a target="_blank" href="'+url+'">Click here</a>',
-          ' to visualize the tree in a new window.',
-          '</p>'].join('');
+        for (var species in thumbs) {
+          var url = thumbs[species];
+          species = species.replace(' ', '_');
+          var regexp = new RegExp(species+'(:[\\d\\.]+)?');
+          url = url.replace('&', '&colon;');
+          console.log(regexp);
+          console.log(species, url);
+          tree = tree.replace(regexp, '$&[&&NHX:img='+url+']');
+        }
 
-        $('#speciesWaiting .modal-body').html(gotText);
+        var el = $([
+          '<div>',
+          '<p>Found the evolutionary tree connecting your species!</p>',
+          '<a class="pgt-link">Click here</a>',
+          ' to open the Newick text.',
+          '</div>'].join(''));
+        var link = el.find('a');
+        link.on('click', function() {
+          // Open the current call script in a new window
+          var openWindow = window.open(undefined, 'pgtPopup', 'width = 500, height = 500');
+          $(openWindow.document.body).html(tree);
 
+        });
+
+        $('#speciesWaiting .modal-body').append(el);
       }
     });
   },
@@ -2407,7 +2434,7 @@ Phylotastic.App = {
   serverBase: function() {
     var url = window.location.href;
     if (url.match(/~greg/i)) {
-      return 'http://'+window.location.host+'/~greg/pgt/cgi-bin/';
+      return 'http://' + window.location.host + '/~greg/pgt/cgi-bin/';
     } else {
       return 'http://phylotastic-wg.nescent.org/~gjuggler/PhyloGeoTastic/cgi-bin/';
     }
@@ -2428,6 +2455,9 @@ $().ready(function() {
   // Click "iNaturalist" and "mammals"
   Phylotastic.DataSources.onDataSourceClick(null, Phylotastic.DataSources.sources[0]);
   Phylotastic.DataSources.onSpeciesClick(null, Phylotastic.DataSources.species[0]);
+
+  var examplesEl = $('.examples-wrap')[0];
+  Phylotastic.DataSources.createExamplesUI(examplesEl);
 
   var buttonEl = $('.go-button-wrap')[0];
   var goButton = $(['<button type="button" class="btn go-btn">',
@@ -2500,6 +2530,16 @@ Phylotastic.Maps = {
     this.geocoder = geocoder;
   },
 
+  getMarkerParams: function(marker) {
+    var me = this;
+    var lat = marker.getPosition().lat();
+    var lng = marker.getPosition().lng();
+    return {
+      latitude: lat,
+      longitude: lng
+    };
+  },
+
   getCircleParams: function(circle) {
     var radius = circle.getRadius();
     var center = circle.getCenter();
@@ -2522,18 +2562,65 @@ Phylotastic.Maps = {
     };
   },
 
+  setCurrentParams: function(params) {
+    this.currentParams = params;
+    console.log("Cur params: ", params);
+  },
+
+  setPoint: function(params) {
+    var me = this;
+    this.setPointSelection();
+    var pointOptions = {
+      position: new google.maps.LatLng(params.latitude, params.longitude),
+      map: this.map,
+      editable: true
+    };
+    var marker = new google.maps.Marker(pointOptions);
+    this.currentOverlay = marker;
+
+    me.setCurrentParams(me.getMarkerParams(marker));
+
+    this.map.panTo(marker.getPosition());
+    this.map.setZoom(params.zoomLevel);
+    this.drawingManager.setOptions({
+      drawingMode: null
+    });
+  },
+
   setCircle: function(params) {
+    var me = this;
+
     this.setCircularSelection();
     var circleOptions = {
       center: new google.maps.LatLng(params.latitude, params.longitude),
       radius: params.radius,
-      map: map,
+      map: this.map,
       editable: true
     };
     var circle = new google.maps.Circle(circleOptions);
+    google.maps.event.addListener(circle, 'radius_changed', function() {
+      me.setCurrentParams(me.getCircleParams(circle));
+    });
+    google.maps.event.addListener(circle, 'center_changed', function() {
+      me.setCurrentParams(me.getCircleParams(circle));
+    });
+
+    me.setCurrentParams(me.getCircleParams(circle));
+
+    this.currentOverlay = circle;
+    if (params.zoomLevel) {
+      this.map.panTo(circle.getBounds());
+      this.map.setZoom(params.zoomLevel);
+    } else {
+      this.map.panToBounds(circle.getBounds());
+    }
+    this.drawingManager.setOptions({
+      drawingMode: null
+    });
   },
 
   setRect: function(params) {
+    var me = this;
     this.setRectangularSelection();
     var rectOptions = {
       map: this.map,
@@ -2542,7 +2629,23 @@ Phylotastic.Maps = {
         new google.maps.LatLng(params.ne_latitude, params.ne_longitude)),
       editable: true
     };
-    var rec = new google.maps.Rectangle(rectOptions);
+    var rect = new google.maps.Rectangle(rectOptions);
+    google.maps.event.addListener(rect, 'bounds_changed', function() {
+      me.setCurrentParams(me.getRectParams(rect));
+    });
+
+    me.setCurrentParams(me.getRectParams(rect));
+
+    this.currentOverlay = rect;
+    if (params.zoomLevel) {
+      this.map.fitBounds(rect.getBounds());
+      this.map.setZoom(params.zoomLevel);
+    } else {
+      this.map.fitBounds(rect.getBounds());
+    }
+    this.drawingManager.setOptions({
+      drawingMode: null
+    });
   },
 
   onShapeEvent: function(event) {
@@ -2555,18 +2658,18 @@ Phylotastic.Maps = {
     }
     if (event.type === google.maps.drawing.OverlayType.CIRCLE) {
       var circle = event.overlay;
-      me.currentParams = me.getCircleParams(circle);
+      me.setCurrentParams(me.getCircleParams(circle));
       google.maps.event.addListener(circle, 'radius_changed', function() {
-        me.currentParams = me.getCircleParams(circle);
+        me.setCurrentParams(me.getCircleParams(circle));
       });
       google.maps.event.addListener(circle, 'center_changed', function() {
-        me.currentParams = me.getCircleParams(circle);
+        me.setCurrentParams(me.getCircleParams(circle));
       });
     } else if (event.type === google.maps.drawing.OverlayType.RECTANGLE) {
       var rect = event.overlay;
-      me.currentParams = me.getRectParams(rect);
+      me.setCurrentParams(me.getRectParams(rect));
       google.maps.event.addListener(rect, 'bounds_changed', function() {
-        me.currentParams = me.getRectParams(rect);
+        me.setCurrentParams(me.getRectParams(rect));
       });
     } else {
       var lat = event.overlay.position.lat();
@@ -2581,12 +2684,6 @@ Phylotastic.Maps = {
       drawingMode: null
     });
     me.currentOverlay = event.overlay;
-  },
-
-  getParams: function() {
-    var currentOverlay = this.currentOverlay;
-
-    console.log(currentOverlay);
   },
 
   centerOnCountry: function(country) {
@@ -2659,25 +2756,97 @@ Phylotastic.DataSources = {
   currentSpecies: undefined,
 
   createExamplesUI: function(el) {
+    var me = this;
+
+    var learnMore = ' <a target="_blank" href="https://docs.google.com/document/d/1vQ1sNOkwUPtJsiN7TSV0EIxPQ0PhinPM_flvs_RNHtU/edit">Click here to learn more</a>';
+
     var examples = [{
       db: 'inaturalist',
       species: 'birds',
-      latitude: 0,
-      longitude: 0,
-      ne_latitude: 0,
-      ne_longitude: 0,
-      details_url: 'http://www.google.com/'
+      type: 'rectangle',
+      latitude: 39.9518,
+      longitude: -111.3354,
+      ne_latitude: 40.96330,
+      ne_longitude: -109.22607,
+      zoomLevel: 6,
+      label: 'Mountain Adaptations',
+      description_short: 'Explore the relationships between birds living at high elevations in the mountains of northern Utah.',
+      description_long: '<b>Mountain Adaptations</b>: Life at higher elevations is likely to be more stressful for birds due to colder temperatures and lack of oxygen.' + learnMore,
     },
     {
-      db: 'lampyr',
+      db: 'inaturalist',
       species: 'mammals',
-      latitude: 0,
-      longitude: 0,
-      ne_latitude: 0,
-      ne_longitude: 0,
-      details_url: 'http://www.google.com/'
+      type: 'rectangle',
+      latitude: -25.7998,
+      longitude: 34.4091796875,
+      ne_latitude: -15.792253,
+      ne_longitude: 50.0537109,
+      zoomLevel: 4,
+      label: 'Island Radiations',
+      description_short: 'Explore the biodiversity within Madagascar and the nearby continental shores of mainland Africa.',
+      description_long: '<b>Island Radiations</b>: Animals that get isolated on islands sometime radiate on that island where one or just a few lineages will form the majority of the diversity on that island.' + learnMore,
+    },
+    {
+      db: 'inaturalist',
+      species: 'plants',
+      type: 'rectangle',
+      latitude: 24.8066,
+      longitude: -82.496,
+      ne_latitude: 26.9416,
+      ne_longitude: -79.4860,
+      zoomLevel: 5,
+      label: 'Evolution of Communities',
+      description_short: 'Compare the tree of life in two very different habitats.',
+      description_long: '<b>Evolution of Communities</b>: Phylogenetic trees can give us a different perspective of communities. Use PhyloGeoTastic to compare the phylogenetic structure of plants identified in Florida and New Mexico.' + learnMore,
     }];
 
+    var buffer = [];
+    examples.forEach(function(example) {
+      var link = $('<a class="pgt-demo-link">' + example.label + '</a>');
+      link.on('click', function(event) {
+        me.onExampleClick(event, example);
+      });
+      link.hover(function(event) {
+        $(link).popover({
+          html: true,
+          content: example.description_short,
+          title: example.label
+        });
+        $(link).popover('show');
+      },
+      function(event) {
+        $(link).popover('destroy');
+      });
+      buffer.push(link);
+    });
+
+    for (var i = 0; i < buffer.length; i++) {
+      $(el).append(buffer[i]);
+      if (i < buffer.length - 1) {
+        $(el).append('<span>, </span>');
+      }
+    }
+  },
+
+  onExampleClick: function(event, example) {
+    // Get the right source.
+    this.currentSource = this.getSourceById(example.db);
+    this.currentSpecies = this.getSpeciesById(example.species);
+
+    Phylotastic.DataSources.onDataSourceClick(null, this.currentSource);
+    Phylotastic.DataSources.onSpeciesClick(null, this.currentSpecies);
+
+    $('#infopanel').html(example.description_long);
+
+    //Phylotastic.App.updateMapToSource();
+    var bounds;
+    if (example.type === 'circle') {
+      Phylotastic.Maps.setCircle(example);
+    } else if (example.type === 'point') {
+      Phylotastic.Maps.setPoint(example);
+    } else {
+      Phylotastic.Maps.setRect(example);
+    }
   },
 
   createDataSourceUI: function(el) {
@@ -2716,7 +2885,7 @@ Phylotastic.DataSources = {
       selectionType: 'point',
       description: 'Find species locations collected from museum records and other sources around the world',
       infoPanel: ['<p>',
-        'The Global Biodiversity Information Facility (<a href="http://www.gbif.org/">GBIF</a>) database contains',
+        'The Global Biodiversity Information Facility (<a href="http://www.gbif.org/">GBIF</a>) database contains ',
         'species that scientists have been found in a given area.',
         '</p>',
         '<p>Use the map tools to drop a marker in your area of interest. We will get a list of the 100 nearest organisms.</p>'].join(''),
@@ -2756,6 +2925,26 @@ Phylotastic.DataSources = {
     //);
     this.currentSource = source;
     Phylotastic.App.updateMapToSource();
+  },
+
+  getSourceById: function(id) {
+    var source;
+    this.sources.forEach(function(x) {
+      if (x.id === id) {
+        source = x;
+      }
+    });
+    return source;
+  },
+
+  getSpeciesById: function(id) {
+    var species;
+    this.species.forEach(function(x) {
+      if (x.id === id) {
+        species = x;
+      }
+    });
+    return species;
   },
 
   onSpeciesClick: function(event, species) {
